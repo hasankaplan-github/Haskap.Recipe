698cf8953ef90decfa7677342e01f1e0570e4f5d
Hasan Kaplan <hasankaplan.kariyer@gmail.com>
2023-12-28 22:19:46 +0300

    Public layout içine kategoriler menüsü eklendi.

---
+++ Haskap.Recipe.Ui.MvcWebUi/ViewComponents/Shared/CategoriesViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Haskap.Recipe.Application.Contracts;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace Haskap.Recipe.Ui.MvcWebUi.ViewComponents.Shared;
+
+public class CategoriesViewComponent : ViewComponent
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesViewComponent(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync(Guid recipeId, CancellationToken cancellationToken = default)
+    {
+        var categories = await _categoryService.GetAllAsync(cancellationToken);
+
+        return View(categories);
+
+    }
+}
